5f1456e03422802f896b0d4e770841025f78e1af
Christian Stamati <50241352+christianstamatistud@users.noreply.github.com>
2019-12-15 15:52:54 +0100

    atrahasis v0.1 ver 2019.3.0f3

---
+++ human-extinction-project/Assets/HumanExtinction/_scripts/ScriptableObjects/PerlinNoiseData.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Atrahasis
+{
+    public enum TransformTarget
+    {
+        Position,
+        Rotation,
+        Both
+    }
+
+    [CreateAssetMenu(fileName = "PerlinNoiseData", menuName = "FirstPersonController/Data/PerlinNoiseData")]
+    public class PerlinNoiseData : ScriptableObject
+    {
+        #region Variables
+        public TransformTarget transformTarget;
+
+        [Space]
+        public float amplitude;
+        public float frequency;
+        #endregion
+
+    }
+
+
+}
+
