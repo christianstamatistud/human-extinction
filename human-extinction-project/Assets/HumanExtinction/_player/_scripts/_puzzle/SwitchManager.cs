a2ac36dbb8b7e1036ecaf7dc880f61816bf89b45
Christian Stamati <50241352+christianstamatistud@users.noreply.github.com>
2020-01-21 08:51:09 +0100

    puzzle1

---
+++ human-extinction-project/Assets/HumanExtinction/_player/_scripts/_puzzle/SwitchManager.cs
@@ -0,0 +1,197 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace CS
+{
+    public class SwitchManager : MonoBehaviour
+    {
+        public delegate void OnResetTerminal();
+        public event OnResetTerminal resetTerminal;
+
+        public static SwitchManager Instance;
+        public string worldToGuess = "fire";
+        public string currentWord = "";
+        public Camera m_camera;
+
+        public Transform[] lights;
+
+        private void Awake()
+        {
+            Instance = this;
+            //inputHandler = FindObjectOfType<InputHandler>();
+        }
+
+        private void OnEnable()
+        {
+            //inputHandler.playerInteract += GuessWord;
+        }
+
+        private void OnDisable()
+        {
+            //inputHandler.playerInteract -= GuessWord;
+
+        }
+
+        private void Update()
+        {
+
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                RaycastHit hit;
+                Ray ray = m_camera.ScreenPointToRay(Input.mousePosition);
+
+                if (Physics.Raycast(ray, out hit))
+                {
+                    if (hit.transform.gameObject.GetComponent<Switcher>() && currentWord.Length <=3)
+                    {
+                        Switcher s;
+                        s = hit.transform.gameObject.GetComponent<Switcher>();
+                        s.SetLetter();
+                    }
+
+                    if (hit.transform.gameObject.GetComponent<SwitchManager>())
+                    {
+                        GuessWord();
+                    }
+
+                }
+
+
+
+            }
+        }
+
+        public void AddLetter(string letter)
+        {
+            currentWord += letter;
+
+        }
+
+        void GuessWord()
+        {
+            if(worldToGuess == currentWord)
+            {
+                Debug.Log("world guess");
+                //all buttons should be green
+            }
+
+            //first letter
+            if(currentWord.Contains("f"))
+            {
+                Debug.Log("contains f");
+                Debug.Log(currentWord.IndexOf("f"));
+
+
+                if(currentWord.IndexOf("f") == 0)
+                {
+                    Renderer r;
+                    r = lights[currentWord.IndexOf("f")].GetComponent<Renderer>();
+                    r.material.SetColor("_BaseColor", Color.green);
+
+                }
+                else
+                {
+                    Renderer r;
+                    r = lights[currentWord.IndexOf("f")].GetComponent<Renderer>();
+                    r.material.SetColor("_BaseColor", Color.red);
+                }
+             
+            }
+
+            //second letter
+            if (currentWord.Contains("i"))
+            {
+                Debug.Log("contains i");
+                Debug.Log(currentWord.IndexOf("i"));
+
+
+                if (currentWord.IndexOf("i") == 1)
+                {
+                    Renderer r;
+                    r = lights[currentWord.IndexOf("i")].GetComponent<Renderer>();
+                    r.material.SetColor("_BaseColor", Color.green);
+
+                }
+                else
+                {
+                    Renderer r;
+                    r = lights[currentWord.IndexOf("i")].GetComponent<Renderer>();
+                    r.material.SetColor("_BaseColor", Color.red);
+                }
+
+            }
+
+            //third letter
+            if (currentWord.Contains("r"))
+            {
+                Debug.Log("contains r");
+                Debug.Log(currentWord.IndexOf("r"));
+
+
+                if (currentWord.IndexOf("r") == 2)
+                {
+                    Renderer r;
+                    r = lights[currentWord.IndexOf("r")].GetComponent<Renderer>();
+                    r.material.SetColor("_BaseColor", Color.green);
+
+                }
+                else
+                {
+                    Renderer r;
+                    r = lights[currentWord.IndexOf("r")].GetComponent<Renderer>();
+                    r.material.SetColor("_BaseColor", Color.red);
+                }
+
+            }
+
+            //fourth letter
+            if (currentWord.Contains("e"))
+            {
+                Debug.Log("contains e");
+                Debug.Log(currentWord.IndexOf("e"));
+
+
+                if (currentWord.IndexOf("e") == 3)
+                {
+                    Renderer r;
+                    r = lights[currentWord.IndexOf("e")].GetComponent<Renderer>();
+                    r.material.SetColor("_BaseColor", Color.green);
+
+                }
+                else
+                {
+                    Renderer r;
+                    r = lights[currentWord.IndexOf("e")].GetComponent<Renderer>();
+                    r.material.SetColor("_BaseColor", Color.red);
+                }
+
+            }
+
+
+            StartCoroutine(ClearObjects());
+
+
+
+
+        }
+
+        IEnumerator ClearObjects()
+        {
+            yield return new WaitForSeconds(3);
+
+            foreach (Transform t in lights)
+            {
+                Renderer r;
+                r = t.GetComponent<Renderer>();
+                r.material.SetColor("_BaseColor", Color.white);
+                currentWord = "";
+                resetTerminal();
+            }
+
+        }
+    }
+
+}
+
---
+++ human-extinction-project/Assets/HumanExtinction/_player/_scripts/_puzzle/Switcher.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CS
+{
+    public class Switcher : MonoBehaviour
+    {
+        public string letter;
+        Renderer r;
+        public SwitchManager sm;
+
+        private void Awake()
+        {
+            r = GetComponentInChildren<Renderer>();
+            sm = FindObjectOfType<SwitchManager>();
+        }
+
+        private void OnEnable()
+        {
+            sm.resetTerminal += ResetSwitcher;
+        }
+        private void OnDisable()
+        {
+            sm.resetTerminal -= ResetSwitcher;
+
+        }
+
+        public void ResetSwitcher()
+        {
+            r.material.color = Color.red;
+        }
+
+
+        public void SetLetter()
+        {
+            SwitchManager.Instance.AddLetter(letter);
+            r.material.color = Color.green;
+        }
+
+
+
+
+    }
+
+}
+
